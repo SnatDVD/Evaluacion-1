f614c976b76d18eef3bd4adb4cea96d74a17e2fa
Vespertino <Vespertino@C8PC08>
2016-11-08 21:49:00 +0100

    David

---
+++ src/com/iescomercio/tema3/elInstituto/Pedrope.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iescomercio.tema3.elInstituto;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Pedrope {
+
+    public Pedrope() {
+    }
+    
+    
+}
